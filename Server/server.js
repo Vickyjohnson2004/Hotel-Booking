e067e99edd8f325f8bd8d52b3592ac01f3aac51e
Victor Johnson <okikevictorodinaka@gmail.com>
2025-12-28 10:50:46 -0800

    cors corrected

--- Server/server.js
+++ Server/server.js
@@ -56,8 +56,10 @@ connectDB();
 // ================= MIDDLEWARES =================
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true, // important for cookies
+    origin: ["http://localhost:5173", "https://quickstay-sigma-ten.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
